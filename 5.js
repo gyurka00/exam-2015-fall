1b893a9c30e1ac6b4fb9897d0bc0e49a4c16f2f6
György Csadó <gyorgy.csado@gmail.com>
2016-01-22 11:14:30 +0100

    add the 5th 'app'

--- 5.js
+++ 5.js
@@ -2,16 +2,46 @@
 
 // Create a constructor called CarStore, that takes an array of cars as a parameter:
 // It should have an "addCar" method that adds a car object to it's list
-// It should have a "getSumPrice" method that returns the sum of all cars price 
+// It should have a "getSumPrice" method that returns the sum of all cars price
 // It should have a "getOldestCarType" method that returns the oldest car's type
 // It should have a "deleteCarByType" method that removes the cars from the inner list
 // that have the given type
 
+function CarStore(carsArray) {
+  this.cars = cars;
+}
 
+CarStore.prototype.addCar = function(carBrand, price, year) {
+  var car = {type: carBrand, price: price, year: year};
+  this.cars.push(car);
+}
 
+CarStore.prototype.getSumPrice = function() {
+  var totalPrice = 0;
+  this.cars.forEach(function(car) {
+    totalPrice += car.price;
+  });
+  return totalPrice;
+}
 
+CarStore.prototype.getOldestCarType = function() {
+  var oldestCar = this.cars[0];
+  this.cars.forEach(function(car) {
+    if (oldestCar.year > car.year ) {
+      oldestCar = car;
+    }
+  });
+  return oldestCar.type;
+}
 
-
+CarStore.prototype.deleteCarByType = function(type) {
+  var newCars = [];
+  this.cars.forEach(function(car) {
+    if (car.type !== type)
+    newCars.push(car);
+  });
+  return this.cars = newCars;
+}
 
 var cars = [
   {type: 'Dodge', price: 20000, year: 2012},
@@ -24,8 +54,7 @@ var cars = [
 var carStore = new CarStore(cars);
 
 carStore.addCar('Smart', 18000, 2011);
-console.log(carStore.getSumPrice()); // 122000 
+console.log(carStore.getSumPrice()); // 122000
 console.log(carStore.getOldestCarType()); // 'Trabant'
 carStore.deleteCarByType('Ferrari');
-console.log(carStore.getSumPrice()); // 82000 
-
+console.log(carStore.getSumPrice()); // 82000
